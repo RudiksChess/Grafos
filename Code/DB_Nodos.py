957559d501b16745f31ff7012e4db4e153c3de9f
Rudik Rompich <rudikrrc@gmail.com>
2021-05-19 17:27:59 -0600

    Nodos conectados con sus relaciones

--- Code/DB_Nodos.py
+++ Code/DB_Nodos.py
@@ -1,28 +1,34 @@
+def creador_nodos_individuales(texto):
+    texto_lower = texto.lower()
+    texto_capitalizado = texto.capitalize()
+    comando = f"({texto_lower}:{texto_capitalizado} {{nombre:\"{texto}\"}})"
+    return comando
+
 class Nodos:
     def __init__(self):
         self.nodos = []
+        self.users = ["user001","user002","user003","user004","user005","user006","user007","user008","user009", "user010",
+                     "user011", "user012", "user013", "user014", "user015", "user016", "user017", "user018", "user019", "user020",
+                     "user021", "user022", "user023", "user024"]
         self.NIVEL = ["Principiante", "Intermedio", "Avanzado"]
         self.PARTE_FAVORITA = ["Apertura", "Medio", "Final"]
         self.PLATAFORMA = ["Chesscom", "Lichess", "Chess24"]
         self.APERTURA = ["Italiana_Espanola", "Inglesa", "Sistema_Londres", "Fianchetto"]
         self.DEFENSA = ["Eslava", "Francesa", "Caro_Kann", "Siciliana"]
+        self.nodos.append(self.users)
         self.nodos.append(self.NIVEL)
         self.nodos.append(self.PARTE_FAVORITA)
         self.nodos.append(self.PLATAFORMA)
         self.nodos.append(self.APERTURA)
         self.nodos.append(self.DEFENSA)
 
-    def creador_nodos_individuales(self, texto):
-        texto_lower = texto.lower()
-        texto_capitalizado = texto.capitalize()
-        comando = f"({texto_lower}:{texto_capitalizado} {{nombre:\"{texto}\"}})"
-        return comando
-
+    @property
     def creador_nodos(self):
-        comando_nodos = "CREATE \n"
+        comando_nodos: str = "CREATE \n"
         for lista in self.nodos:
             for elemento in lista:
-                comando_nodos += self.creador_nodos_individuales(elemento) + ",\n"
+                comando_nodos += creador_nodos_individuales(elemento) + ",\n"
         return comando_nodos[:-2]
 
-print(Nodos().creador_nodos())
+
+print(Nodos().creador_nodos)
--- Code/Separador.py
+++ Code/Separador.py
@@ -5,7 +5,11 @@ class Separador:
     @property
     def retonar(self):
         USER, NIVEL_BLITZ, NIVEL_RAPIDAS, PARTE_FAVORITA, PLATAFORMA, APERTURA, DEFENSA = self.separado
-        return USER
-
-
-print(Separador("user001,Principiante,Intermedio,Medio,Chess.com,Inglesa,Siciliana").retonar)
+        Nodo_NIVEL_BLITZ =f"MATCH {USER}:{USER.capitalize()} MERGE ({USER})-[:NIVEL_BLITZ]->({NIVEL_BLITZ.lower()})"
+        Nodo_NIVEL_RAPIDAS = f"MATCH {USER}:{USER.capitalize()} MERGE ({USER})-[:NIVEL_RAPIDAS]->({NIVEL_RAPIDAS.lower()})"
+        Nodo_PARTE_FAVORITA = f"MATCH {USER}:{USER.capitalize()} MERGE ({USER})-[:PARTE_FAVORITA]->({PARTE_FAVORITA.lower()})"
+        Nodo_PLATAFORMA = f"MATCH {USER}:{USER.capitalize()} MERGE ({USER})-[:PLATAFORMA]->({PLATAFORMA.lower()})"
+        Nodo_APERTURA = f"MATCH {USER}:{USER.capitalize()} MERGE ({USER})-[:APERTURA]->({APERTURA.lower()})"
+        Nodo_DEFENSA = f"MATCH {USER}:{USER.capitalize()} MERGE ({USER})-[:DEFENSA]->({DEFENSA.lower()})"
+        Connection = Nodo_NIVEL_BLITZ +";\n"+Nodo_NIVEL_RAPIDAS +";\n"+  Nodo_PARTE_FAVORITA +";\n"+  Nodo_PLATAFORMA +";\n"+Nodo_APERTURA +";\n"+ Nodo_DEFENSA
+        return Connection
